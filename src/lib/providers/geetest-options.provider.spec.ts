44ee56e734a89cff66be14312fdcfb239e8a1475
noctifer20 <ohanjanyan.miqayel@gmail.com>
2021-09-06 15:49:36 +0400

    fix(tests): add unit test bypass checking sync flow configuration

--- src/lib/providers/geetest-options.provider.spec.ts
+++ src/lib/providers/geetest-options.provider.spec.ts
@@ -31,13 +31,56 @@ test('should be defined', (t) => {
   t.not(geetestOptionsProvider, undefined);
 });
 
-test('should export options with default values', (t) => {
+test('should export options with default values for bypass async flow', (t) => {
   t.deepEqual(geetestOptionsProvider.options, {
     geetestId: 'GEETEST_ID',
     geetestKey: 'GEETEST_KEY',
     bypassConfig: {
       url: 'https://bypass.geetest.com/v1/bypass_status.php',
       policy: 'onDemand',
+    },
+    apiServer: 'https://api.geetest.com',
+    registerUrl: '/register.php',
+    validateUrl: '/validate.php',
+    jsonFormat: '1',
+    httpTimeoutDefault: 5000,
+    version: 'node-express:3.1.1',
+    geetestChallengeKey: 'geetest_challenge',
+    geetestValidateKey: 'geetest_validate',
+    geetestSeccodeKey: 'geetest_seccode',
+    debug: false,
+  });
+});
+
+test('should export options with default values for bypass sync flow', async (t) => {
+  const module: TestingModule = await Test.createTestingModule({
+    providers: [
+      {
+        provide: GEETEST_OPTIONS,
+        useValue: {
+          geetestId: 'GEETEST_ID',
+          geetestKey: 'GEETEST_KEY',
+          bypassConfig: {
+            url: 'https://bypass.geetest.com/v1/bypass_status.php',
+            policy: 'polling',
+            frequency: 10000,
+          },
+        } as GeetestModuleOptions,
+      },
+      GeetestOptionsProvider,
+    ],
+  }).compile();
+
+  geetestOptionsProvider = module.get<GeetestOptionsProvider>(
+    GeetestOptionsProvider
+  );
+  t.deepEqual(geetestOptionsProvider.options, {
+    geetestId: 'GEETEST_ID',
+    geetestKey: 'GEETEST_KEY',
+    bypassConfig: {
+      url: 'https://bypass.geetest.com/v1/bypass_status.php',
+      policy: 'polling',
+      frequency: 10000,
     },
     apiServer: 'https://api.geetest.com',
     registerUrl: '/register.php',
