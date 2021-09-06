2e1103dbb91d65117e98be80528ad3c191058373
noctifer20 <ohanjanyan.miqayel@gmail.com>
2021-09-06 19:24:13 +0400

    fix(hotfix): export guards

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/geetest.module';
 export * from './lib/interfaces';
 export * from './lib/services';
+export * from './lib/guards';
---
+++ src/lib/guards/index.ts
@@ -0,0 +1,1 @@
+export * from './geetest-verify.guard';
